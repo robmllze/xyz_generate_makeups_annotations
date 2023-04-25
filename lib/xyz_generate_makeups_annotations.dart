37142e02583b143f6e8632ed26bd838e2ab7443f
Robert Mollentze <robmllze@gmail.com>
2023-04-25 02:30:07 -0400

    first commit

---
+++ lib/xyz_generate_makeups_annotations.dart
@@ -0,0 +1,20 @@
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//
+// XYZ Generate Makeups
+//
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+
+library xyz_generate_makeups_annotations;
+
+class GenerateMakeups {
+  final Set<String> names;
+  final Map<String, String> parameters;
+  const GenerateMakeups({
+    this.names = const {},
+    this.parameters = const {},
+  });
+}
+
+class MakeupParameter {
+  const MakeupParameter();
+}
